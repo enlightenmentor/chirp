dbe7638bfa127279d5a98d649cb47fc33f844bef
Tom Fales <fales.tom.1994@gmail.com>
2021-11-07 19:00:53 +0200

    serialisable dependency removed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export { default as encryptPassword } from './encryptPassword'
-export { default as serialisable } from './serialisable'
 export { default as validatePassword } from './validatePassword'
